90358643f07fd2a2ee4594e02375310e29a09b47
Niranjan Ilawe <niranjan.ilawe@10xgenomics.com>
2022-05-25 09:12:46 -0700

    updated email sending to validate names and email

--- pyqc/send_error_emails.py
+++ pyqc/send_error_emails.py
@@ -3,16 +3,24 @@ import smtplib
 from email.mime.text import MIMEText
 
 qc_email_dict = {
-    "ilawe.niranjan": "niranjan.ilawe@10xgenomics.com",
+    "Admin": "niranjan.ilawe@10xgenomics.com",
     "Supervisor": "niranjan.ilawe@10xgenomics.com",
 }
 
 
 def send_error_emails(error_list, filename, qc_by):
 
+    # check validatity of qc_by name and email
+    try:
+        qc_tech_name = qc_by.split(".")[1].capitalize()
+        qc_by_email = f"{qc_by}@10xgenomics.com"
+    except:
+        qc_tech_name = qc_by
+        qc_by_email = "niranjan.ilawe@10xgenomics.com"        
+
     # creating body
     body = f"""
-    Hello {qc_by.split(".")[1].capitalize()}, 
+    Hello {qc_tech_name}, 
     I could not parse the QC123 file: {filename}, you uploaded to Box recently.
     Following errors were found: {error_list}.
     Please follow the recommended practices to ensure proper upload of this data.
@@ -25,14 +33,14 @@ def send_error_emails(error_list, filename, qc_by):
     """
 
     # creating a list of people the emails are sent too
-    receivers = [qc_email_dict[qc_by], qc_email_dict["Supervisor"]]
+    receivers = [qc_by_email, qc_email_dict["Supervisor"]]
 
     # converting string to a MIMEtext object. This was necessary since this was the only way
     # the email was able to distinguish between the subject, to, and from
     msg = MIMEText(body)
     msg["Subject"] = "QC123 File Auto-ingestion Error"
     msg["From"] = "auto-file-parser@10xgenomics.com"
-    msg["To"] = f"{qc_email_dict[qc_by]}, {qc_email_dict['Supervisor']}"
+    msg["To"] = f"{qc_by_email}, {qc_email_dict['Supervisor']}"
 
     s = smtplib.SMTP("smtp.google.com")
 
